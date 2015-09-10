55e203d990381a18ac19ff3d9f449e5488b05159
masonlin <a1807d@yahoo.com.tw>
2015-09-10 08:07:42 +0800

    V1.0.1

--- mason_profile.js
+++ mason_profile.js
@@ -5,37 +5,40 @@
 * @version V1.0.1 2015/09/09
 */
 // create server.
-var app = require('express').createServer(),
+//var app = require('express').createServer(),
+var app = require('express')(),
 port = process.env.PORT || 8080,
 users = [
-          {name: 'Mason'},
-          {name: 'Stuart'}
+          {name: 'Mason'},   //:id=0
+          {name: 'Stuart'}   //:id=1
         ];
 app.listen(port);
 
-app.all('/user/:id/:op?', function(req, res, next){
+//app.all('/user/:id/:op?', function(req, res, next){
+app.all('/user/:id', function(req, res, next){
                               req.user = users[req.params.id];
                               if (req.user) {
                                   next();
                               } else {
-                                  next(new Error('cannot find user' + req.params.id));
+                                  next(new Error('cannot find user ' + req.params.id));
                               }
                           });
 
 app.get('/user/:id', function(req, res){
-                         res.send('viewing' + req.user.name);
+                         res.send('viewing ' + req.user.name);
                      });
 
 app.get('/user/:id/edit', function(req, res){
-                              res.send('editing' + req.user.name);
+                              res.send('editing ' + req.user.name);
                           });
 
 app.get('/user/:id/delete', function(req, res){
-                                res.send(’deleting ’ + req.user.name);
+                                res.send('deleting ' + req.user.name);
                             });
 
 app.get('*', function(req, res){
-                 res.send(’Page not found!’, 404);
+                 //res.send('Page not found!', 404);
+                 res.status(400).send('Page not found!');
              });
 
-console.log(’start express server\n’);
+console.log('start express server\n');
