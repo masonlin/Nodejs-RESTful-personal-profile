81f1b90c1b742f19bcb6bf6ba0690a6ef0c7c6aa
Mason Lin (賣森林) <a1807d@yahoo.com.tw>
2016-07-01 14:48:02 +0800

    upgrade20160701

--- mason_profile.js
+++ mason_profile.js
@@ -14,8 +14,8 @@ users = [
           {name: 'Jessica'}   //:id=2
         ];
 app.listen(port);
-var name = "";
 
+//>>>>>>>>>> http get + routes >>>>>>>>>>
 //app.all('/user/:id/:op?', function(req, res, next){
 //usage: http://127.0.0.1:8080/user/0
 app.all('/user/:id', function(req, res, next){
@@ -49,9 +49,37 @@ app.get('/user/:id/delete', function(req, res){
                                 res.send('deleting ' + req.user.name);
                             });
 
+
+app.get('/', function(req, res){
+    //>>>>>>>>>> html >>>>>>>>>>
+    app.use(require('express').static(require('path').join(__dirname, '/public')));  //以下這4行用來載入public路徑下的靜態文件如html檔
+    app.set('views', __dirname + '/public');
+    app.engine('html', require('ejs').renderFile);
+    app.set('view engine', 'html');
+
+    res.render('node_profile_post_http.html');
+    //<<<<<<<<<< html <<<<<<<<<<
+});
+
+
 app.get('*', function(req, res){
                  //res.send('Page not found!', 404);
                  res.status(400).send('Page not found!');
              });
+
+//<<<<<<<<<< http get + routes <<<<<<<<<<
+
+//>>>>>>>>>> http post + routes >>>>>>>>>>
+app.use(require('body-parser').urlencoded({extended:true}));   //用來解析HTML，之後req才會有資料
+app.post('/post_test', function(req, res, next){
+    //res.send('hi, ' + req.query.name);
+    //res.send('your education backgroud: ' + req.query.education);
+    //res.send('your education backgroud: ' + req.param('education'));
+    res.send('hi, ' + req.body.name + ', your education backgroud:  ' + req.body.education);
+});
+//<<<<<<<<<< http post + routes <<<<<<<<<<
+
+
+
 
 console.log('start express server\n');
