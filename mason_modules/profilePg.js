df4d205632bbcecb2005293019f8f077e0eee8db
Mason Lin (賣森林) <104554013@cc.ncu.edu.tw>
2016-07-23 15:42:02 +0800

    for Heroku

--- mason_modules/profilePg.js
+++ mason_modules/profilePg.js
@@ -1,11 +1,30 @@
+/**
+* @overview
+*
+* @author Mason Lin
+* @version v1.0.0 2016/07/13 Original
+*          v1.1.0 2016/07/21 modify for pre-release
+*/
+
 var pg = require('pg');
+var clsMasonConf = require('./masonconf');
+var oMasonConf = new clsMasonConf();
+
 
 var result;
 
 var ClsPG = function(res, profileId){
-  this.conStr = process.env.DATABASE_URL || 'Postgres://postgres:aabbcc11@localhost:5432/profiledb';
+  // this.conStr = process.env.DATABASE_URL || 'Postgres://postgres:aabbcc11@localhost:5432/profiledb';
+
+  if(oMasonConf.isDev == true){
+    this.conStr = process.env.DATABASE_URL || 'Postgres://postgres:aabbcc11@localhost:5432/profiledb';
+  }else{
+    this.conStr = process.env.DATABASE_URL || 'postgres://uhiovunxefljkw:NOBbE94z3FQq0IliteWmz9Xg2V@ec2-54-163-228-0.compute-1.amazonaws.com:5432/dj6thr6fi7vmc';
+    pg.defaults.ssl = true;  //heroku SSL
+  }
 
   this.getData = function(){
+
       pg.connect(this.conStr, function(error, client, done){
         if(error){
           done();
@@ -13,7 +32,13 @@ var ClsPG = function(res, profileId){
           return 'connetion fail!';
         }
 
-        var qryStr = 'SELECT * FROM profile WHERE id=' + profileId + ';';
+        //var qryStr = 'SELECT * FROM profile WHERE id=' + profileId + ';';
+        if(oMasonConf.isDev == true){
+          var qryStr = 'SELECT * FROM profile WHERE id=' + profileId + ';';
+        }else{
+          var qryStr = 'SELECT * FROM mason_info WHERE col1=\'' + profileId + '\';';
+        }
+
         var query = client.query(qryStr);
 
         query.on('row', function(row, result){
